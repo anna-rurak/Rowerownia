10edc9127e58d53bd0656159c576cb5527b37858
anna-rurak <annarurakk@gmail.com>
2024-04-04 12:42:10 +0200

    updating

--- index.js
+++ index.js
@@ -42,3 +42,49 @@ techSpecTab.forEach((element) => {
 });
 
 // --------- GENEROWANIE TABLICY ELEMENTÓW ---------
+const cardContainer = document.getElementById("card-container");
+const newCard = `<section class="col d-inline-block">
+                    <div class="card text-center border-dark">
+                        <div class="card-header text-bg-dark">
+                            <ul class="nav nav-tabs card-header-tabs">
+                                <li class="nav-item">
+                                    <a class="nav-link summary-tab active" href="#">Przegląd</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tech-spec-tab" href="#">Dane techniczne</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="card-body text-start">
+                            <div class="d-inline-block row content">
+                                <section class="summary-card">
+                                    <img class="image col" src="./assets/1.jpg" alt="" />
+                                    <div class="card-body col px-3">
+                                        <h7 class="category lighter">- trekkingowy -</h7>
+                                        <h3 class="card-title my-3">Rower nr 1</h3>
+                                        <p class="card-text">Rower miejski z 3-rzędowymi przerzutkami i nóżką. Komfortowa jazda po mieście.</p>
+                                        <!-- <a href="#" class="choose-btn btn btn-dark">Wybierz</a> -->
+                                    </div>
+                                </section>
+                                <section class="tech-spec-card d-none">
+                                    <div class="card-body col my-0">
+                                        <h5 class="card-title my-3">Rower nr 1</h5>
+                                        <!-- <hr class="line" /> -->
+                                        <p class="card-text py-3 my-0 border-bottom border-warning">Marka: Romet</p>
+                                        <!-- <hr class="line" /> -->
+                                        <p class="card-text py-3 my-0 border-bottom border-warning">Model: SX5230</p>
+                                        <!-- <hr class="line" /> -->
+                                        <p class="card-text py-3 my-0 border-bottom border-warning">Typ: trekkingowy</p>
+                                        <!-- <hr class="line" /> -->
+                                        <p class="card-text py-3 my-0 border-bottom border-warning">Rozmiar ramy: 57 cm</p>
+                                        <!-- <hr class="line" /> -->
+                                        <p class="card-text py-3 my-0 border-bottom border-warning">Waga: 15 kg</p>
+                                    </div>
+                                </section>
+                            </div>
+                            <a href="#" class="choose-btn btn btn-dark">Wybierz</a>
+                        </div>
+                    </div>
+                </section>`;
+
+for (let i = 0; i < bikes.length; i++) {}
