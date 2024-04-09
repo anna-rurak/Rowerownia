39dc98ffd270faf619f8008c2fac24356547702a
anna-rurak <annarurakk@gmail.com>
2024-04-09 16:12:33 +0200

    updating files with the ones from stackblitz

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
+import "./style.css";
 import { bikes } from "./data.js";
 
-console.log(bikes);
-const chooseButtons = document.querySelectorAll(".choose-btn ");
+// --------- GENEROWANIE TABLICY ELEMENTÓW ---------
+const cardContainer = document.getElementById("card-container");
+
+for (let i = 0; i < bikes.length; i++) {
+    const newCard = `<section class="col d-inline-block my-4 d-none">
+<div class="card text-center border-dark">
+    <div class="card-header text-bg-dark">
+        <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
+                <a class="nav-link summary-tab active" href="#">Przegląd</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link tech-spec-tab" href="#">Dane techniczne</a>
+            </li>
+        </ul>
+    </div>
+    <div class="card-body text-start">
+        <div class="d-inline-block row content">
+            <section class="summary-card">
+                <img class="image col" src="${bikes[i].img}" alt="" />
+                <div class="card-body col px-3">
+                    <h7 class="category lighter">- ${bikes[i].type} -</h7>
+                    <h3 class="card-title my-3">${bikes[i].name}</h3>
+                    <p class="card-text">${bikes[i].info}</p>
+                </div>
+            </section>
+            <section class="tech-spec-card d-none">
+                <div class="card-body col my-0">
+                    <h5 class="card-title my-3">${bikes[i].name}</h5>
+                    <!-- <hr class="line" /> -->
+                    <p class="card-text py-3 my-0 border-bottom border-warning">Marka: ${bikes[i].producer}</p>
+                    <!-- <hr class="line" /> -->
+                    <p class="card-text py-3 my-0 border-bottom border-warning">Typ: ${bikes[i].type}</p>
+                    <!-- <hr class="line" /> -->
+                    <p class="card-text py-3 my-0 border-bottom border-warning">Rozmiar koła: ${bikes[i].wheelSize}</p>
+                    <!-- <hr class="line" /> -->
+                    <p class="card-text py-3 my-0 border-bottom border-warning">Rozmiar ramy: ${bikes[i].frameSize} cm</p>
+                    <!-- <hr class="line" /> -->
+                    <p class="card-text py-3 my-0 border-bottom border-warning">Waga: ${bikes[i].weight} kg</p>
+                </div>
+            </section>
+        </div>
+        <a href="#" class="choose-btn btn btn-dark" id='element${i}'>Wybierz</a>
+    </div>
+</div>
+</section>`;
+
+    cardContainer.insertAdjacentHTML("beforeend", newCard);
+}
+
+// --------- OBSŁUGA KART PRZEGLĄD / DANE TECHNICZNE NA KARCIE ELEMENTU ---------
 const summaryTab = document.querySelectorAll(".summary-tab");
 const techSpecTab = document.querySelectorAll(".tech-spec-tab");
 
-// --------- OBSŁUGA KART PRZEGLĄD / DANE TECHNICZNE NA KARCIE ELEMENTU ---------
-
 summaryTab.forEach((element) => {
     element.addEventListener("click", () => {
+        console.log("hi");
         const cardHeader = element.closest(".card-header");
         const techSpec = cardHeader.querySelector(".tech-spec-tab");
         const colParent = element.closest(".col");
@@ -41,50 +90,13 @@ techSpecTab.forEach((element) => {
     });
 });
 
-// --------- GENEROWANIE TABLICY ELEMENTÓW ---------
-const cardContainer = document.getElementById("card-container");
-const newCard = `<section class="col d-inline-block">
-                    <div class="card text-center border-dark">
-                        <div class="card-header text-bg-dark">
-                            <ul class="nav nav-tabs card-header-tabs">
-                                <li class="nav-item">
-                                    <a class="nav-link summary-tab active" href="#">Przegląd</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link tech-spec-tab" href="#">Dane techniczne</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="card-body text-start">
-                            <div class="d-inline-block row content">
-                                <section class="summary-card">
-                                    <img class="image col" src="./assets/1.jpg" alt="" />
-                                    <div class="card-body col px-3">
-                                        <h7 class="category lighter">- trekkingowy -</h7>
-                                        <h3 class="card-title my-3">Rower nr 1</h3>
-                                        <p class="card-text">Rower miejski z 3-rzędowymi przerzutkami i nóżką. Komfortowa jazda po mieście.</p>
-                                        <!-- <a href="#" class="choose-btn btn btn-dark">Wybierz</a> -->
-                                    </div>
-                                </section>
-                                <section class="tech-spec-card d-none">
-                                    <div class="card-body col my-0">
-                                        <h5 class="card-title my-3">Rower nr 1</h5>
-                                        <!-- <hr class="line" /> -->
-                                        <p class="card-text py-3 my-0 border-bottom border-warning">Marka: Romet</p>
-                                        <!-- <hr class="line" /> -->
-                                        <p class="card-text py-3 my-0 border-bottom border-warning">Model: SX5230</p>
-                                        <!-- <hr class="line" /> -->
-                                        <p class="card-text py-3 my-0 border-bottom border-warning">Typ: trekkingowy</p>
-                                        <!-- <hr class="line" /> -->
-                                        <p class="card-text py-3 my-0 border-bottom border-warning">Rozmiar ramy: 57 cm</p>
-                                        <!-- <hr class="line" /> -->
-                                        <p class="card-text py-3 my-0 border-bottom border-warning">Waga: 15 kg</p>
-                                    </div>
-                                </section>
-                            </div>
-                            <a href="#" class="choose-btn btn btn-dark">Wybierz</a>
-                        </div>
-                    </div>
-                </section>`;
+// --------- CHOOSING ELEMENT ---------
+const userChoice = [];
+const chooseButtons = document.querySelectorAll(".choose-btn");
 
-for (let i = 0; i < bikes.length; i++) {}
+chooseButtons.forEach((element) => {
+    element.addEventListener("click", () => {
+        const bikeIndex = element.getAttribute("id").slice(-1);
+        console.log(bikeIndex);
+    });
+});
