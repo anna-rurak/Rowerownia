8ff6607f9c306399f6b6c24ebbaf427caa2ad37d
anna-rurak <anna.rurakk@gmail.com>
2024-04-29 18:51:40 +0200

    updating files

--- scripts/formPage.js
+++ scripts/formPage.js
@@ -5,15 +5,13 @@ const $nameError = document.getElementById("error-message-name");
 const $emailInput = document.getElementById("email-input");
 const $emailError = document.getElementById("error-message-email");
 const $radioButton1 = document.getElementById("flexRadioDefault1");
-const $radioButton2 = document.getElementById("flexRadioDefault2");
-const $radioButtons = document.querySelectorAll(".form-check-input");
-const $additionalAccessories = document.querySelectorAll(".accessories");
+const $radioButtons = document.querySelectorAll('input[type="radio"][name="flexRadioDefault"]');
 const $startDateInput = document.getElementById("start-date");
 const $startDateMsg = document.getElementById("start-date-msg");
 const $endDateInput = document.getElementById("end-date");
 const $endDateMsg = document.getElementById("end-date-msg");
+const $additionals = document.querySelectorAll(".accessories");
 const $finalPrice = document.getElementById("final-price");
-const $finalizationButton = document.getElementById("submit-btn");
 const $bikeName = document.getElementById("bike-name");
 const $bikeImg = document.getElementById("bike-img");
 const $bikeProducer = document.getElementById("p-producer");
@@ -22,63 +20,41 @@ const $bikeWheelSize = document.getElementById("p-wheelSize");
 const $bikeFrameSize = document.getElementById("p-frameSize");
 const $bikeWeight = document.getElementById("p-weight");
 const $bikePrice = document.getElementById("p-price");
-const unreadableChoice = localStorage.getItem("chosenBikeId");
-const userChoice = JSON.parse(unreadableChoice);
-const defaultName = JSON.parse(localStorage.getItem("userName"));
-const defaultEmail = JSON.parse(localStorage.getItem("userEmail"));
-// const defaultRadio = JSON.parse(localStorage.getItem("paymentMethod"));
 
 //-------------------------------------------------
 //   ŁADOWANIE DANYCH Z LOCAL STORAGE
 //-------------------------------------------------
 
-if (defaultName != "" || defaultName != null) {
-    $nameInput.value = defaultName;
-}
+const userChoice = JSON.parse(localStorage.getItem("chosenBikeId"));
+const storedName = JSON.parse(localStorage.getItem("userName"));
+const storedEmail = JSON.parse(localStorage.getItem("userEmail"));
+const storedPaymentMethod = JSON.parse(localStorage.getItem("paymentMethod"));
+let additionalsArray = JSON.parse(localStorage.getItem("additionals"));
 
-if (defaultEmail != "" || defaultEmail != null) {
-    $emailInput.value = defaultEmail;
-}
+//-------------------------------------------------
+//   SPECYFIKACJA TECHNICZNA
+//-------------------------------------------------
 
 $bikeName.innerText = bikes[userChoice].name;
 $bikeImg.setAttribute("src", bikes[userChoice].img);
-
 $bikeProducer.innerText = `Producent: ${bikes[userChoice].producer}`;
 $bikeType.innerText = `Typ: ${bikes[userChoice].type}`;
 $bikeWheelSize.innerText = `Rozmiar koła: ${bikes[userChoice].wheelSize}`;
 $bikeFrameSize.innerText = `Rozmiar ramy: ${bikes[userChoice].frameSize}`;
 $bikeWeight.innerText = `Waga: ${bikes[userChoice].weight}kg`;
 $bikePrice.innerText = `Cena: ${bikes[userChoice].price},00 zł / dzień`;
-
-//-------------------------------------------------
-// USTAWIENIE DANYCH DOMYŚLNYCH
-//-------------------------------------------------
-
-function setSpecifictDate(num) {
-    const today = new Date();
-    today.setDate(today.getDate() + num);
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, "0");
-    const day = String(today.getDate()).padStart(2, "0");
-    const initialDate = `${year}-${month}-${day}`;
-    return initialDate;
-}
-
-function setSpecifictDateX(myDay, num) {
-    myDay.setDate(myDay.getDate() + num);
-    const year = day.getFullYear();
-    const month = String(myDay.getMonth() + 1).padStart(2, "0");
-    const day = String(myDay.getDate()).padStart(2, "0");
-    const initialDate = `${year}-${month}-${day}`;
-    return initialDate;
-}
-
-$startDateInput.value = setSpecifictDate(7);
-$endDateInput.value = setSpecifictDate(7);
+$finalPrice.innerHTML = `${bikes[userChoice].price},00 zł`;
 
 //-------------------------------------------------
 //  WALIDACJA INPUTÓW
 //-------------------------------------------------
+if (storedName != "" || storedName != null) {
+    $nameInput.value = storedName;
+}
+
+if (storedEmail != "" || storedEmail != null) {
+    $emailInput.value = storedEmail;
+}
 
 $nameInput.addEventListener("change", () => {
     const name = $nameInput.value;
@@ -104,24 +80,44 @@ $emailInput.addEventListener("change", () => {
     }
 });
 
-//!!!!!!!!!!!!!!!!!!TRZEBA TO NAPRAWIĆ!!!!!!!!!!!!!!!!!!
-// $radioButtons.forEach((el) => {
-//     el.addEventListener("click", () => {
-//         localStorage.setItem("paymentMethod", JSON.stringify(el.getAttribute("id")));
-//     });
-// });
+//-------------------------------------------------
+//  WYBÓR FORMY PŁATNOŚCI
+//-------------------------------------------------
+
+if (storedPaymentMethod != "" || storedPaymentMethod != null) {
+    localStorage.setItem("paymentMethod", JSON.stringify($radioButton1.value));
+}
+
+$radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener("change", () => {
+        localStorage.setItem("paymentMethod", JSON.stringify(radioButton.value));
+    });
+});
 
 //---------------------------------------------------------
 //  WYBÓR DATY ROZPOCZĘCIA I ZAKOŃCZENIA NAJMU ROWERU
 //---------------------------------------------------------
 
+function setSpecifictDate(num) {
+    const today = new Date();
+    today.setDate(today.getDate() + num);
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    const initialDate = `${year}-${month}-${day}`;
+    return initialDate;
+}
+
 function checkStartDate(startDate) {
-    if (startDate < new Date(setSpecifictDate(7))) {
+    startDate = new Date(startDate);
+    let startDateScope = new Date(setSpecifictDate(7));
+    if (startDate < startDateScope) {
         $startDateMsg.style.boxShadow = "5px 5px 15px 6px #892514";
-        $startDateInput.value = setSpecifictDate(7);
+        $startDateInput.value = startDateScope.toISOString().slice(0, 10); // Format "YYYY-MM-DD"
+        $endDateInput.value = $startDateInput.value;
     } else {
         $startDateMsg.style.boxShadow = "";
-        $endDateInput.value = startDate;
+        $endDateInput.value = $startDateInput.value;
     }
 }
 
@@ -141,36 +137,71 @@ function checkEndDate(endDate) {
 }
 
 function countDays() {
-    const daysCount = Math.ceil((new Date($endDateInput.value) - new Date($startDateInput.value)) / (1000 * 60 * 60 * 24));
+    const daysCount = Math.ceil((new Date($endDateInput.value) - new Date($startDateInput.value)) / (1000 * 60 * 60 * 24) + 1);
     return daysCount;
 }
 
-$startDateInput.addEventListener("change", () => {
-    const startDate = $startDateInput.value;
+$startDateInput.value = setSpecifictDate(7); //ustawia najwcześniejszą możliwą datę rozpoczęcia najmu
+$endDateInput.value = setSpecifictDate(7); //ustawia najkrótszy możliwy czas najmu (1 dzień)
+
+$startDateInput.addEventListener("change", (e) => {
+    const startDate = e.target.value;
     checkStartDate(startDate);
     priceUpdate();
 });
 
-$endDateInput.addEventListener("change", () => {
-    const endDate = $endDateInput.value;
+$endDateInput.addEventListener("change", (e) => {
+    const endDate = e.target.value;
     checkEndDate(endDate);
     priceUpdate();
+});
+
+//---------------------------------------------------------
+//  DODAWANIE AKCESORIÓW
+//---------------------------------------------------------
+
+if (additionalsArray === null || additionalsArray === "") {
+    additionalsArray = [];
+}
+
+for (let i = 0; i < $additionals.length; i++) {
+    if (additionalsArray.includes($additionals[i].getAttribute("id"))) {
+        $additionals[i].setAttribute("checked", "checked");
+    }
+}
+
+$additionals.forEach((item) => {
+    item.addEventListener("click", () => {
+        const itemId = item.getAttribute("id");
+        const index = additionalsArray.indexOf(itemId);
+
+        if (item.checked && index === -1) {
+            additionalsArray.push(itemId);
+        } else if (!item.checked && index !== -1) {
+            additionalsArray.splice(index, 1);
+        }
+        localStorage.setItem("additionals", JSON.stringify(additionalsArray));
+        priceUpdate();
+    });
 });
 
 //---------------------------------------------------------
 //  KALKULACJA CENY NAJMU
 //---------------------------------------------------------
 
-let additionalsArray = [];
-
-// DODAĆ WYBÓR DODATKOWYCH AKCESORIÓW
-
 function priceUpdate() {
     let additionalsCost = 0;
     for (let i = 0; i < additionalsArray.length; i++) {
-        additionalsCost = +additionalsArray[i];
+        if (additionalsArray[i] === "kask") {
+            additionalsCost = additionalsCost + 15;
+        } else if (additionalsArray[i] === "blokada") {
+            additionalsCost = additionalsCost + 10;
+        } else if (additionalsArray[i] === "koszyk") {
+            additionalsCost = additionalsCost + 12;
+        }
     }
 
     const summary = (bikes[userChoice].price + additionalsCost) * countDays();
     $finalPrice.innerHTML = `${summary},00 zł`;
+    localStorage.setItem("finalRentalPrice", JSON.stringify(summary));
 }
--- scripts/summaryPage.js
+++ scripts/summaryPage.js
@@ -1,1 +1,28 @@
-import { bikes } from "..data.js";
+import { bikes } from "../data.js";
+
+//---------------------------------------------------------
+//  POBIERANIE DANYCH Z LOCAL STORAGE
+//---------------------------------------------------------
+const userChoice = JSON.parse(localStorage.getItem("chosenBikeId"));
+console.log(typeof userChoice);
+const paymentMethod = JSON.parse(localStorage.getItem("paymentMethod"));
+const additionals = JSON.parse(localStorage.getItem("additionals"));
+const finalRentalPrice = JSON.parse(localStorage.getItem("finalRentalPrice"));
+
+//---------------------------------------------------------
+//  TWORZENIE ZMIENNYCH DOM
+//---------------------------------------------------------
+
+const $paymentMethod = document.getElementById("payment-method");
+const $finalRentalPrice = document.getElementById("final-rental-price");
+const $bikeName = document.getElementById("bike-name");
+const $bikeImg = document.getElementById("bike-img");
+
+$paymentMethod.innerText = paymentMethod;
+$finalRentalPrice.innerText = `${finalRentalPrice},00 zł`;
+$bikeName.innerText = bikes[userChoice].name;
+$bikeImg.setAttribute("src", bikes[userChoice].img);
+
+//TODO
+// dodać przycisk - "kolejne zamówienie"
+// dodać resetowanie local storage po zamknięciu przeglądarki lub jeśli ktoś wchodzi na stronę po raz pierwszy
